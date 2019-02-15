dd23ded444e16c7ae2d8b86cee414a29d66febc4
fakorede <abiolafakorede@gmail.com>
2019-02-16 00:11:01 +0100

    config for infura

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+require("dotenv").config();
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -6,6 +9,14 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY
+        ),
+      network_id: 4
     }
   }
 };
