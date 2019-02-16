a19d4a300cbe1bc4508c472b6d204bb7f0f1c7b1
fakorede <abiolafakorede@gmail.com>
2019-02-16 12:18:39 +0100

    another commit

--- migrations/1550261809_deploy_contract.js
+++ migrations/1550261809_deploy_contract.js
@@ -1,4 +1,4 @@
-var Adoption = artifacts.require("Adoption");
+var Adoption = artifacts.require("./Adoption.sol");
 
 module.exports = function(deployer) {
   // Use deployer to state migration tasks.
